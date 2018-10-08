79bba9fec4aa30294a6ce1ff7c9b32e044a32f5f
Bruno Martins <bgcngm@gmail.com>
2018-10-08 10:39:02 +0100

    angler: remove config.fs duplicates
    Fixes:
    vendor/pa/config/config.fs device/huawei/angler/config.fs"
    Duplicate path "persist/" found in files: "vendor/pa/config/config.fs" and device/huawei/angler/config.fs
    
    Change-Id: Ia16910dccc01f8e7b15f642791c5bd7d6bb47c94

--- config.fs
+++ config.fs
@@ -8,13 +8,3 @@ mode: 0700
 user: AID_CAMERA
 group: AID_SHELL
 caps: SYS_NICE
-[persist/]
-mode: 0771
-user: AID_SYSTEM
-group: AID_SYSTEM
-caps: 0
-[firmware/]
-mode: 0771
-user: AID_SYSTEM
-group: AID_SYSTEM
-caps: 0
